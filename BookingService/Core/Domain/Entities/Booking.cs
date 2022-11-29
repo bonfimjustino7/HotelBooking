4632cd21058f731c7b851faf4a2be3166d345d17
bonfim.justino7 <bonfim1999@gmail.com>
2022-11-29 15:17:37 -0300

    feat: criando migrations de booking e guest

--- BookingService/Core/Domain/Entities/Booking.cs
+++ BookingService/Core/Domain/Entities/Booking.cs
@@ -15,6 +15,9 @@ namespace Domain.Entities
         public DateTime PlaceAt { get; set; }   
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
+        public Room Room { get; set; }
+        public Guest Guest { get; set; }    
+
         private Status Status { get; set; }
 
         public Status CurrentStatus
